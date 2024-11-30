41528ebdcefbbc8cd5c9662531eb2bfcc0b044e0
Félix <delassus.felix@gmail.com>
2024-11-30 21:13:45 +0100

    feat(*): pull request

---
+++ src/main/java/Projet_POO/Logger.java
@@ -0,0 +1,22 @@
+package Projet_POO;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Logger {
+    private FileWriter writer;
+
+    public Logger(String fileName) throws IOException {
+        this.writer = new FileWriter(fileName, true);
+    }
+
+    public void log(String message) throws IOException {
+        writer.write(message + "\n");
+        writer.flush();
+    }
+
+    public void close() throws IOException {
+        writer.close();
+    }
+}
+
--- src/main/java/Projet_POO/Partie.java
+++ src/main/java/Projet_POO/Partie.java
@@ -1,57 +1,79 @@
 package Projet_POO;
 
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 public class Partie {
-    Date dateDebut, dateFin;
-    boolean gameOver, enCours;
-    ArrayList<Carte> cartes;
+    private Joueur joueur;
+    private Carte carte;
+    private List<Personnage> ennemis;
+    private Logger logger;
 
-    public Partie(Date dateDebut, boolean gameOver, boolean enCours, ArrayList<Carte> cartes) {
-        this.dateDebut = dateDebut;
-        this.gameOver = gameOver;
-        this.enCours = enCours;
-        this.cartes = cartes;
+    public Partie(Joueur hero, Carte carte, Logger logger) {
+        this.joueur = hero;
+        this.carte = carte;
+        this.logger = logger;
+        this.ennemis = new ArrayList<>();
     }
 
-    public Date getDateDebut() {
-        return dateDebut;
+    // Méthode pour générer des ennemis sur le parcours
+    public void genererEnnemis() {
+        Random rand = new Random();
+        for (int i = 0; i < carte.getPositionActuelle(); i++) {
+            if (rand.nextInt(10) < 4) { // 40% de chance d'ajouter un ennemi
+                Brigand ennemi = new Brigand();  // Ajoutez ici d'autres types d'ennemis
+                ennemis.add(ennemi);
+            }
+        }
     }
 
-    public void setDateDebut(Date dateDebut) {
-        this.dateDebut = dateDebut;
+    // Méthode pour gérer le combat entre le héros et un ennemi
+    public void combattre(Personnage ennemi) throws Exception {
+        logger.log("Le héros commence à combattre un ennemi.");
+
+        // Le héros attaque en premier
+        joueur.attaquer(ennemi);
+        if (!ennemi.estVivant()) {
+            logger.log("L'ennemi a été vaincu.");
+            ennemis.remove(ennemi);
+        } else {
+            // L'ennemi attaque ensuite
+            ennemi.attaquer(hero);
+            if (!joueur.estVivant()) {
+                logger.log("Le héros est mort.");
+                throw new Exception("Le héros est mort.");
+            }
+        }
     }
 
-    public Date getDateFin() {
-        return dateFin;
-    }
+    // Méthode pour faire avancer le jeu
+    public void jouer() throws Exception {
+        while (!carte.estArrivee() && joueur.estVivant()) {
+            // Avancer sur la carte
+            carte.avancer();
+            logger.log("Le héros avance. Position actuelle : " + carte.getPositionActuelle());
 
-    public void setDateFin(Date dateFin) {
-        this.dateFin = dateFin;
-    }
+            // Le héros peut rencontrer des ennemis
+            if (ennemis.size() > 0) {
+                Personnage ennemi = ennemis.get(0); // Rencontre avec le premier ennemi
+                logger.log("Le héros rencontre un ennemi.");
+                combattre(ennemi);
+            }
 
-    public boolean isGameOver() {
-        return gameOver;
-    }
+            // Logique de victoire (si on atteint la fin de la carte)
+            if (carte.estArrivee()) {
+                logger.log("Le héros a atteint la fin de la carte et a gagné !");
+                break;
+            }
+        }
 
-    public void setGameOver(boolean gameOver) {
-        this.gameOver = gameOver;
-    }
+        // Si le héros est mort avant la fin
+        if (!joueur.estVivant()) {
+            logger.log("Le héros a perdu !");
+        }
 
-    public boolean isEnCours() {
-        return enCours;
-    }
 
-    public void setEnCours(boolean enCours) {
-        this.enCours = enCours;
-    }
 
-    public ArrayList getCartes() {
-        return cartes;
-    }
-
-    public void setCartes(ArrayList cartes) {
-        this.cartes = cartes;
     }
 }
